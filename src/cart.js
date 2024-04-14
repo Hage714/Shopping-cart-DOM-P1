d8737025e5eb4c198322d86b4a9f5f7587ebcfe2
Hage <hagewoche99@gmail.com>
2024-04-14 11:44:18 +0300

    completing the shoppingcart project

--- src/cart.js
+++ src/cart.js
@@ -23,7 +23,7 @@ let calculation = () => {
 if (basket.length !== 0){
     return shoppingCart.innerHTML = basket
     .map((x) =>{
-        console.log(x);
+        
         let { id, item } = x;
         let search = shopItemsData.find((y) => y.id === id) || [];
         return `
@@ -35,12 +35,16 @@ if (basket.length !== 0){
             <p>${search.name}</p>
             <p class="cart-item-price">$ ${search.price}</p>
           </h4>
-          <i class="bi bi-x-lg"></i>
+          <i onclick="removeItem (${id})" class="bi bi-x-lg"></i>
           </div>
+          
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">${item}</div>
+            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+        </div>
 
-          <div class ="cart-buttons"></div>
-
-          <h3></h3>
+          <h3>$ ${item * search.price}</h3>
         </div>
         </div>
         `;
@@ -58,3 +62,86 @@ else{
 }
   }
   generateCartItems(); //invoking the function
+
+  let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id) //passing x as an argument so that it checks all the objects one by one
+  
+    if (search === undefined) {
+      basket.push({
+        id: selectedItem.id,
+        item: 1,
+      })
+    } else {
+      search.item += 1;
+    }
+  
+    //pushing the basket inside the local storage so that the browser can remember it what has been selected
+    localStorage.setItem("data", JSON.stringify(basket))
+    generateCartItems();
+    update(selectedItem.id);
+  };
+  
+  let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id) //passing x as an argument so that it checks all the objects one by one
+  
+    if (search === undefined) return;
+   else if (search.item === 0) return; //it shouldn't go beyond zero items, return statement stops the process.
+       else {
+      search.item -= 1;
+    }
+    
+    
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0);  //selecting all the objects that has no zero on the item
+    generateCartItems();
+   localStorage.setItem("data", JSON.stringify(basket))
+  };
+
+  let update = (id) => {
+    let search = basket.find((x) => x.id === id) //quick search iff the item exists,the number will increase
+    //console.log(search.item)
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+    totalAmount()  //invoking the function so it can update the total amount on the total bill
+  };
+
+  let removeItem = (id) => {
+let selectedItem = id;
+//console.log(selectedItem.id)
+basket = basket.filter((x) => x.id !== selectedItem.id) //whenever you click on the cross,the item will be removed from the basket cart and update the basket
+generateCartItems();
+totalAmount()  //invoking the function
+calculation();
+localStorage.setItem("data", JSON.stringify(basket))
+  };
+
+  //clearing the entire cart
+  let clearCart = () => {
+basket = []
+generateCartItems()  //removing all the items from the cart
+calculation();
+  localStorage.setItem("data", JSON.stringify(basket))  //updating the local storage
+}
+
+  let totalAmount = ()=>{
+    if (basket.length !== 0) {
+      let amount = basket.map((x)=>{
+        let {item, id } = x  //destructuring x
+        let search = shopItemsData.find((y) => y.id === id) || []; //using the id to match the database
+        return item * search.price;
+      }).reduce((x, y) => x + y, 0)   //adding the totals together
+      //console.log(amount);
+      label.innerHTML = `
+      <h2>Total Bill: $ ${amount}</h2>
+      <button class="checkout">Checkout</button>
+      <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+      `;
+    }
+    else return
+  }
+  totalAmount()  //invoking the function
+
+
+  
